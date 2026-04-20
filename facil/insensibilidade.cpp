8d21c59b6530d790bedbd7a18d9c77933e245e97
pablo costa <pabloemanuelgamespb@gmail.com>
2026-04-20 18:12:46 -0300

    Add distance calculation program in insensibilidade.cpp
    Implement distance calculation between points in 2D space.

---
+++ facil/insensibilidade.cpp
@@ -0,0 +1,30 @@
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+ll dist(ll x1, ll x2, ll y1, ll y2){
+    
+    ll dx = x1 - x2;
+    ll dy = y1 - y2;
+
+    return dx*dx + dy*dy;
+}
+
+int main(){
+
+    int n; cin >> n;
+
+    ll d = 0;
+
+    for(int i = 0; i < n; i++){
+
+        ll x1, y1, x2, y2; cin >> x1 >> y1 >> x2 >> y2;
+        d += dist(x1, x2, y1, y2);
+        
+    }
+
+    cout << d << endl;
+}
