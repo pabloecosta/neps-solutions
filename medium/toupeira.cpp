e81a02672275923f71452f12e766aab9e39ba8df
pablo costa <pabloemanuelgamespb@gmail.com>
2026-04-20 10:50:32 -0300

    Create toupeira.cpp

---
+++ medium/toupeira.cpp
@@ -0,0 +1,52 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<unordered_set<int>> sala;
+int ans = 0;
+
+bool dentro(int u, int v){
+
+    return sala[u].count(v);
+
+}
+
+int main(){
+
+    int s, t; cin >> s >> t;
+
+    sala.resize(s+1);
+
+    for(int i = 0; i < t; i++){
+
+        int a, b; cin >> a >> b;
+
+        sala[a].insert(b);
+        sala[b].insert(a);
+
+    }
+
+    int p; cin >> p;
+
+    for(int i = 0; i < p; i++){
+
+        int n; cin >> n;
+        vector<int> passeio(n);
+        bool vai = true;
+
+        for(int i = 0; i < n; i++){
+            cin >> passeio[i];
+        }
+
+        for(int i = 0; i < n-1; i++){
+            if(!dentro(passeio[i], passeio[i+1])){
+                vai = false;
+            }
+        }
+
+        if(vai) ans++;
+
+    }
+
+    cout << ans << endl;
+}
