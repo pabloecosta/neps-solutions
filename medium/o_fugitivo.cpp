9b2c50944c52a059b85db7d3096e4404b3ce52f2
pablo costa <pabloemanuelgamespb@gmail.com>
2026-04-22 21:08:54 -0300

    Create o_fugitivo.cpp

---
+++ medium/o_fugitivo.cpp
@@ -0,0 +1,37 @@
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+int main(){
+
+    ll n, m; cin >> n >> m;
+
+    bool saiu = false;
+
+    ll dist_max = m*m;
+    pair<ll, ll> P;
+    ll dist_atual = 0;
+
+    for(ll i = 0; i < n; i++){
+
+        char c; cin >> c;
+        ll d; cin >> d;
+
+        if(c=='N') P.first += d;
+        else if(c=='S') P.first -= d;
+        else if(c=='L') P.second += d;
+        else P.second -= d;
+
+        dist_atual = P.first*P.first + P.second*P.second;
+
+        if(dist_atual > dist_max){
+            saiu = true;
+        }
+    }
+
+    if(saiu) cout << 1 << endl;
+    else cout << 0 << endl;
+}
